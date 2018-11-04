d81ccb8d6080667bfb9731f515f437ca0aa3c60c
John A Maloney <john.a.maloney@gmail.com>
2018-11-04 14:08:07 -0600

    Mobile nav

---
+++ site/content/gallery/garage.md
@@ -0,0 +1,8 @@
+---
+title: garage
+before_image: /img/gallery/garage_before.jpg 
+before_caption: garage remodel before
+after_image: /img/gallery/garage_after.jpg
+after_caption: garage remodel after
+---
+
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,18 @@
 // JS Goes here - ES6 supported
 
 Splitting();
+
+let isOpen = false;
+
+$('.mobile-nav-menu').on('click', (e, target) => {
+  if (isOpen) {
+    $('.navigation').css('right', '-1000px');
+    $('.close-button').css('display', 'none');
+    $('.mobile-nav-menu').html('MENU');
+  } else {
+    $('.navigation').css('right', 0);
+    $('.close-button').css('display', 'block');
+    $('.mobile-nav-menu').html('X');
+  }
+  isOpen = !isOpen;
+})
